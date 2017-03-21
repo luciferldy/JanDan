d78d4032dca25d43dddc99656e9ccc7a70ce8d0b
luciferldy <2460572865@qq.com>
2017-03-21 09:11:45 +0800

    可以显示内容

---
+++ app/src/main/java/io/luciferldy/jandan/behavior/ShrinkBehavior.java
@@ -0,0 +1,10 @@
+package io.luciferldy.jandan.behavior;
+
+import android.support.design.widget.CoordinatorLayout;
+
+/**
+ * Created by Lucifer on 2017/3/20.
+ */
+
+public class ShrinkBehavior extends CoordinatorLayout.Behavior {
+}
